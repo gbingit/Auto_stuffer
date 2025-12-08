3d8fbf556d54cc7e8d5661514436655d92d9e1e5
Edison_gheng <xiaobogeng39@gmail.com>
2025-12-08 14:21:31 +0800

    add new argu--work_sheet

---
+++ cell_value_concatenator.py
@@ -0,0 +1,6 @@
+import openpyxl as xl
+def cell_value_concatenator(file_path: str,
+                            work_sheet: str,
+                            cell1: str,
+                            cell2: str,
+                            output_cell: str = ''):
--- info_snatcher.py
+++ info_snatcher.py
@@ -1,8 +1,10 @@
 
 import openpyxl as xl
-def info_snatcher(path, info_name, cat_row, search_row):
+def info_snatcher(path: str,
+                  work_sheet: str,
+                  info_name, cat_row, search_row):
     wb_on_hand = xl.load_workbook(path)
-    ws_on_hand = wb_on_hand.active
+    ws_on_hand = wb_on_hand[work_sheet]
     for index, cell in enumerate(ws_on_hand[cat_row]):
         if cell.value == info_name:
             target_index = index + 1 # 这是因为 enumerate 是从 0 开始计数，但 Excel 的列是从 1 开始计数，所以需要index + 1来转换。
@@ -11,9 +13,10 @@ def info_snatcher(path, info_name, cat_row, search_row):
 
 if __name__ == "__main__":
     test_path = "/Users/edison/Desktop/J716 S2F/DVT2/FATP/C/stuff 练手/PWZM DVT2c Build Rules V5.xlsx"
+    work_sheet = 'HVE 11_7 - J716C DVT-2 Build ru'
     test_info_name = "FSTP"
     test_cat_row = 2
     test_search_row = 6
-    print(info_snatcher(test_path, test_info_name, test_cat_row, test_search_row))
+    print(info_snatcher(test_path, work_sheet, test_info_name, test_cat_row, test_search_row))
 
 
